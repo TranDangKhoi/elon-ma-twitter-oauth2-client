bd4a60c00a1ee9646cef7042c1e01caa1aae524d
TranDangKhoi <baits16@gmail.com>
2023-12-01 00:34:32 +0700

    :+1: Successfully redirect to google login screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <div>Home</div>;
+  const redirectToGoogleOAuthServer = () => {
+    const url = "https://accounts.google.com/o/oauth2/v2/auth";
+    const query = {
+      client_id: import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID,
+      redirect_uri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
+      response_type: "code",
+      scope: [
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email",
+      ].join(" "),
+      // prompt: "consent",
+    };
+    const queryString = new URLSearchParams(query).toString();
+    return `${url}?${queryString}`;
+  };
+
+  const googleOauthRedirectUrl = redirectToGoogleOAuthServer();
+
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <Link to={googleOauthRedirectUrl}>Login with Google</Link>
+    </div>
+  );
 }
